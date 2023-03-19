7f69a3947428068967939aac21d7548bdbca983e
Kenneth Lamb <55679262+slurrps-mcgee@users.noreply.github.com>
2023-03-19 19:27:52 -0400

    Add new route to the help page will be changing home page to describe the template with examples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HelpComponentComponent } from './help-component/help-component.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'help',
+    component: HelpComponentComponent
   }
 ];
 
